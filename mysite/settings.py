16a04b70d7885eee288eaefbd902f6888a9c2ad3
sereginamaria <60785083+sereginamaria@users.noreply.github.com>
2020-09-23 20:17:43 +0300

    Settings updated

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mysite.web'
+    'web.apps.WebConfig'
 ]
 
 MIDDLEWARE = [
